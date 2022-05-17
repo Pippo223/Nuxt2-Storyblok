f6714a9b120576cd17e3a7877d74e22d7c400e12
Philip Owusu Asare <philip.asare@amalitech.org>
2022-05-17 08:22:30 +0000

    brought storyblok into nuxt

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -2,18 +2,19 @@
   <div>
     <Header />
     <nuxt-logo></nuxt-logo>
+    <Main />
     <Footer />
   </div>
 </template>
 
 <script>
-import Header from '../components/Header.vue'
-import Footer from '../components/Footer.vue'
+import Header from './Header.vue'
+import Footer from './Footer.vue'
 
 export default {
   components: {
     Header,
-    Footer
+    Footer,
   }
 }
 </script>
